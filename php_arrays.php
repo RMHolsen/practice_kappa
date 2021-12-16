0a159aac7a1c4a6d6d0a29347fcca5b2bbdc9320
rmholsen <jaguar.kitty@gmail.com>
2021-12-15 21:26:39 -0500

    Arrays and hashes

---
+++ php_arrays.php
@@ -0,0 +1,21 @@
+<?php 
+
+// this is an array
+
+$array = ["Item One", "Item Two", "Item Three"];
+
+// this is also an array!
+
+$other_array = array("Thing One", "Thing Two", "Cat in a Hat");
+
+// you can do nested arrays too but we're not even going to go there because nested arrays are evil
+
+// ordered arrays are otherwise known as hashes. Remember hashes? We hate them now.
+
+$ordered_array = [
+    "Stupid hashes" => "Ugh"
+    "You only say that because" => "You haven't used them enough to become fluent"
+    "100 hours of hashes" => "You will be fluent"
+];
+
+// no I'm still gonna hate hashes. forever. 
